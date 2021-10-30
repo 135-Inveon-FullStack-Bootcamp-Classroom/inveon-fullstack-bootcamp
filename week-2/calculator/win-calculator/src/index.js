4600ff6c461a1a50633ce0475661f3af94acadc7
Murat <murat@mdasocial.com>
2021-10-30 13:02:30 +0300

    theme impl

--- week-2/calculator/win-calculator/src/index.js
+++ week-2/calculator/win-calculator/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { CalcProvider } from "./CalcContext";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CalcProvider theme="dark">
+      <App />
+    </CalcProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
