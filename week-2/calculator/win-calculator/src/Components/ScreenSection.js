a46933071d23ad506c288048890615f6499b8396
Murat <murat@mdasocial.com>
2021-10-30 11:23:55 +0300

    screen section caption added

--- week-2/calculator/win-calculator/src/Components/ScreenSection.js
+++ week-2/calculator/win-calculator/src/Components/ScreenSection.js
@@ -3,18 +3,27 @@ const styles = {
     width: "100%",
     height: "30%",
     display: "flex",
-    justifyContent: "flex-end",
+    justifyContent: "center",
+    flexDirection: "column",
+    alignItems: "flex-end",
   },
   mainText: {
     color: "#fff",
-    fontSize: 30,
+    fontSize: 40,
     padding: 10,
+    fontWeight: "bold",
+  },
+  caption: {
+    color: "#999",
+    fontSize: 13,
+    paddingRight: 10,
   },
 };
 
 const ScreenSection = () => {
   return (
     <div style={styles.screenSection}>
+      <span style={styles.caption}>25 +</span>
       <span style={styles.mainText}>5</span>
     </div>
   );
