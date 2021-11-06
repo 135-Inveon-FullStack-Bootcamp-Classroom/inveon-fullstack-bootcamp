dffe128befefba4bfc341283319eeed460cfdbb0
Murat <murat@mdasocial.com>
2021-11-06 14:11:33 +0300

    useEffect

---
+++ week-4/caps-generator/src/Components/MemeCard.js
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+export default function MemCard({ img, title }) {
+  return (
+    <Card sx={{ maxWidth: 300 }}>
+      <CardActionArea>
+        <CardMedia component="img" image={img} alt={title} />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
