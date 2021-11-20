3aa17b6aab10b39bc714f4209465410f163c1d8c
Murat <murat@mdasocial.com>
2021-11-20 11:15:15 +0300

    dependency injection start project

---
+++ week-6/DependencyInjectionExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependencyInjectionExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
